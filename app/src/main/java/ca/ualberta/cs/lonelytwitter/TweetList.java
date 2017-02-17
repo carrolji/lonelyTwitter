06d32c73298f41956aeaa0b1077409f2de6973bf
Carrol Jirakul <jirakul@ualberta.ca>
2017-02-16 21:55:04 -0700

    lab 6 exercise

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -10,6 +10,9 @@ public class TweetList {
     private ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
 
     public void add(NormalTweet tweet) {
+        if(tweets.contains(tweet)){
+            throw new IllegalArgumentException();
+        }
         tweets.add(tweet);
     }
 
@@ -24,7 +27,7 @@ public class TweetList {
     }
 
     public void delete(NormalTweet tweet){
-
+        tweets.remove(tweet);
     }
 
     public int getCount(){
